bd0ad72ea3888342ace6041440ad18c53fcd1f09
Frank Castro <frank.castro.g@tecsup.edu.pe>
2025-11-30 18:32:24 -0500

    listo redis

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -o errexit
 pip install -r requirements.txt          # Instala dependencias
 python manage.py collectstatic --no-input  # Recopila archivos estáticos
 # Las migraciones se ejecutarán manualmente después del despliegue
-# python manage.py migrate
+python manage.py migrate
