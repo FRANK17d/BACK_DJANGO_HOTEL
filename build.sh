78642f9cb1d91b56fbbfc880ca9267ef83c9c0da
Frank Castro <frank.castro.g@tecsup.edu.pe>
2025-11-30 20:14:44 -0500

    agregue para ejecutar seeders

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ pip install -r requirements.txt          # Instala dependencias
 python manage.py collectstatic --no-input  # Recopila archivos estáticos
 # Las migraciones se ejecutarán manualmente después del despliegue
 python manage.py migrate
+python manage.py seed_rooms
